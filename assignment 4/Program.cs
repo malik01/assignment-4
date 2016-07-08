d5618a62a7b081ce8ff24a2c328a88c3a2f1d4f1
shahz <shahz@DESKTOP-1LBMKH3>
2016-07-08 03:25:49 -0400

    started my work made loops

--- assignment 4/Program.cs
+++ assignment 4/Program.cs
@@ -10,6 +10,24 @@ namespace assignment_4
     {
         static void Main(string[] args)
         {
+            
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+            Random end = new Random();
+            int rnd = end.Next(1, 6);
+            rnd = array[rnd];
+            foreach (int s  in array)
+            {
+                
+                Console.Write(s);     
+            }
+            
+            for (int i = 1; i < array.Length; i++)
+            {
+
+                Console.WriteLine(rnd);
+                
+            }
+            
         }
     }
 }
